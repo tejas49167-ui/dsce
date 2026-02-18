eecd19efa03b39c50cc3ba5025e980517066b0fb
Tejas <tejas@kali.Tejas>
2026-02-18 16:00:52 +0530

    commit

--- src/feature.py
+++ src/feature.py
@@ -1,10 +1,4 @@
 import pandas as pd
 
-# Load structured logs
 df = pd.read_csv("../data/processed/logs.csv")
-
-# Convert status to integer (important)
-
-print(df.head())
-print(df.tail())
-print(df.isna().sum())
+#still !
